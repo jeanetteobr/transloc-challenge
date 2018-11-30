e4461447c58aca96f2c5a82eef1d29437f7323b6
jeanetteobr <jeanette.obr@gmail.com>
2018-11-30 01:44:00 -0500

    heatmap layer rendering over base map layer. heatmap vizualization only rendering over eastern hemisphere. Set bounding box over Durham.

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
-var testData = {
-  max: 8,
-  data: $.getJSON('ip.json', function (data) {
-    $(data.info).each(function (idx, val) {
-      console.log(val)
-      return val
-    })
+var testData = []
+$.getJSON('ip.json', function (data) {
+  $(data.info).each(function (idx, val) {
+      var array = []
+      array.push(val.lat, val.lng, val.intensity)
+      testData.push(array)
+      console.log(testData)
+    
   })
-}
-
-console.log(testData)
+})
 
 var myMap = L.map('mapid').setView([35.9940, -78.8986], 15)
 
-var tiles = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamVhbmV0dGVvYnIiLCJhIjoiY2pva2hwMDgyMGcydzNybW81djUyd3gxMiJ9.Yjd3JwPb5Y_Q4yMh_HOOzg', {
+var baseLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamVhbmV0dGVvYnIiLCJhIjoiY2pva2hwMDgyMGcydzNybW81djUyd3gxMiJ9.Yjd3JwPb5Y_Q4yMh_HOOzg', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 20,
+  maxZoom: 18,
   id: 'mapbox.streets',
   accessToken: 'your.mapbox.access.token'
 }).addTo(myMap)
 
-var heat = L.heatLayer(testData).addTo(myMap)
+var heat = L.heatLayer(testData, {
+  radius: 20,
+  blur: 50,
+  maxZoom: 1
+}).addTo(myMap)
 
+var corner1 = L.latLng(36.136929, -78.762172),
+  corner2 = L.latLng(35.866725 , -79.007650),
+  bounds = L.latLngBounds(corner1, corner2);
 
+L.rectangle(bounds, {color: "#f44242", weight: 1}).addTo(myMap)
+console.log(bounds)
