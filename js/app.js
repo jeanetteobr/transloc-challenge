9c91900bf85288a3f49b952f1b2417c18866a165
jeanetteobr <jeanette.obr@gmail.com>
2018-11-30 00:15:45 -0500

    move app.js out of js dir. modified package-lock.json with heatmap, leaflet and leaflet-heat dependencies

--- js/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  console.log('hello')
-  $.getJSON('ip.json', function (data) {
-    $.each(data.coordinates, function (key, val) {
-      console.log(val.coordinates)
-    })
-  })
-})
